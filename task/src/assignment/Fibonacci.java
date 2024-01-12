08823bbe3576a2f7280c48b07cddb8cff9166512
Srijana Raut <srijanarautofficial@gmail.com>
2024-01-12 09:08:03 -0600

    Java Basics Code
    Simple Classes example, Private , Protected and so on.

---
+++ task/src/assignment/Fibonacci.java
@@ -0,0 +1,18 @@
+package assignment;
+
+public class Fibonacci {
+	public static void main(String[] args) {
+		int a1=0, a2=1, a3, i , count = 10;
+//		System.out.println(a1+"+a2");
+		
+		for (i=0; i<count; i++) {
+			a3 = a1 +a2;
+			System.out.println(""+a3);
+			a1=a2;
+			a2 = a3;
+			
+		}
+
+	}
+
+}
