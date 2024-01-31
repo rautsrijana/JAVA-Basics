5278dd3fbf69691acd1740db403b20e7ae04c36e
Srijana Raut <srijanarautofficial@gmail.com>
2024-01-31 10:39:44 -0600

    Database Connect
    Build the manual connection with mysql and Insert the data to the database.

---
+++ ThothitWeb1/src/com/thothitweb/Insertrecord.java
@@ -0,0 +1,44 @@
+package com.thothitweb;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class Insertrecord {
+
+	public static void main(String[] args) {
+		
+		try {
+			//step1: load the driver
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("driver is loaded");
+			
+			
+			//step 2: create the connection
+	Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/thothitweb_db","root","Shree@18");
+	System.out.println("connection has been created");
+	
+	//step 3: fire the query
+	String insertquery="insert into users(username,password,email) values(?,?,?)";
+	PreparedStatement ps= conn.prepareStatement(insertquery);
+	ps.setString(1, "Sriana");
+	ps.setString(2, "srija123");
+	ps.setString(3, "srijana@gmail.com");
+
+	int i =ps.executeUpdate();
+	if(i>0) {
+		System.out.println("record has been inserted");
+	}else {
+		System.out.println("record has not been inserted");
+	}
+		}
+	
+	catch(Exception e) {
+		e.printStackTrace();
+	}
+		}
+		
+
+	}
+
+
---
+++ ThothitWeb1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module ThothitWeb1 {
+	requires java.sql;
+}
